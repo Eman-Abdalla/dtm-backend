dc4d9d5a9727cd886d36754526c9bd93932cbbfd
eman.a <eman.a@sybertechnology.com>
2021-05-20 13:46:59 +0200

    added endpoints for multiple fileds in db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 //define port
 const port=3000;
 
-app.get("/get-states-location", (request, response) => {
+app.get("/foriegn_get-states-location", (request, response) => {
     const req=request.query
     con.query('SELECT state,locality, coordinated_lat, coordinated_lng FROM foriegn_dataset', (err,rows) => {
       if(err) throw err;
@@ -23,6 +23,81 @@ app.get("/get-states-location", (request, response) => {
     });
     
     })
+
+    /** get IDPs count */
+    app.get("/idps_total_count", (request, response) => {
+      const req=request.query
+      con.query('SELECT (SUM(less_than_1_male )+SUM(less_than_1_female )+SUM(from_1_5_male)+SUM(from_1_5_female)+SUM(from_6_17_male)+SUM(from_6_17_female)+SUM(from_18_59_male)+SUM(from_18_59_female)+SUM(over_60_male)+SUM(over_60_female)) AS Total FROM idps_dataset ', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+      
+       /** get IDPs states count */
+    app.get("/idps_total_states", (request, response) => {
+      const req=request.query
+      con.query('SELECT DISTINCT COUNT(state_admin1) FROM idps_dataset WHERE 1', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+
+
+            /** get IDPs location count */
+    app.get("/idps_total_locations", (request, response) => {
+      const req=request.query
+      con.query('SELECT DISTINCT COUNT(location_admin3) FROM idps_dataset WHERE 1', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+
+                /** get returnees location count */
+    app.get("/returnees_total_location", (request, response) => {
+      const req=request.query
+      con.query('SELECT DISTINCT COUNT(location_admin3) FROM permenant_returnees  WHERE 1', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+
+                   /** get returnees location count */
+    app.get("/returnees_count", (request, response) => {
+      const req=request.query
+      con.query('SELECT DISTINCT SUM(returnees_ind) FROM permenant_returnees  WHERE 1', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+
+                       /** get returnees location count */
+    app.get("/returnees_states_count", (request, response) => {
+      const req=request.query
+      con.query('SELECT DISTINCT SUM(state_admin1) FROM permenant_returnees  WHERE 1', (err,rows) => {
+        if(err) throw err;
+      
+        response.json({data:rows})
+      
+      });
+      
+      })
+
+      
 
 
 
